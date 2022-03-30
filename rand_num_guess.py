d2ca6269a26b9922adc17d8b2c327207e3a7f312
Ubuntu <ubuntu@ip-172-31-17-170.ec2.internal>
2022-03-30 13:15:38 +0000

    Added colors

--- rand_num_guess.py
+++ rand_num_guess.py
@@ -4,8 +4,11 @@
 # Date: 03/30/2022
 # Description: This program generates a random number that
 # the user must guess correctly to win the game
+# now with funny colors
 
 import math
+import colorama
+from colorama import Fore, Back, Style
 import random
 import sys
 
@@ -17,18 +20,25 @@ def main():
         # Creating the random number
         r_num = random.randint(1, 15)
         print(
-            "I just generated a random number between 1-15. Can you Guess it correctly?"
+            Fore.WHITE
+            + "I just generated a random number between 1-15. Can you Guess it correctly?"
         )
         print(r_num)
 
         # Loop allows user to keep guessing until they get it right
         while True:
-            u_num = int(input("Input your number below:\n"))
+            u_num = int(input(Fore.WHITE + "Input your number below:\n"))
 
             # Comparing user number with random number
             if u_num == r_num:
-                print("Congratulations, you guessed the number correctly!!")
-                answer = input("Would you like to play again? (y/n):").strip().lower()
+                print(
+                    Fore.GREEN + "Congratulations, you guessed the number correctly!!"
+                )
+                answer = (
+                    input(Fore.WHITE + "Would you like to play again? (y/n):")
+                    .strip()
+                    .lower()
+                )
 
                 # what to do with user answer
                 if answer == "y":
@@ -40,7 +50,7 @@ def main():
                     sys.exit()
 
             else:
-                print("Uh oh, wrong answer. Guess again!")
+                print(Fore.RED + "Uh oh, wrong answer. Guess again!")
 
 
 if __name__ == "__main__":
